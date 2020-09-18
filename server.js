7bdc0cbdfb231167b8b11ed95d9713aafbd109dd
revthi@gmail.com <58027828+rey-vthi@users.noreply.github.com>
2020-09-18 11:19:19 +0530

    added api to app

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const {app} = require('./src/app');
 
 const port = process.env.PORT || 8000;
+
 app.listen(port, () => console.log(`listening on ${port}`));
--- src/app.js
+++ src/app.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 
+const allRecipes = [
+  {
+    name: 'pongal',
+    ingredients: ['2cup rice', '2cup jaggery', 'water'],
+    steps: [
+      'Boil water',
+      'put rice into boiled water',
+      'smash the jaggery and add it to boiled rice'
+    ],
+    type: 'sweet'
+  }
+];
+
 const app = express();
 
 app.use(cookieParser());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.json({message: 'hello'});
+app.get('/api/getAllRecipes', (req, res) => {
+  res.json({allRecipes});
 });
 
 module.exports = {app};
