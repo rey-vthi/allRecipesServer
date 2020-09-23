0b3ac6f4f755874a93994d366521e22388ad5d50
revthi@gmail.com <58027828+rey-vthi@users.noreply.github.com>
2020-09-23 21:16:22 +0530

    added github login

--- src/app.js
+++ src/app.js
@@ -1,23 +1,35 @@
+const fs = require('fs');
 const express = require('express');
 const fileUpload = require('express-fileupload');
 const cookieParser = require('cookie-parser');
-const fs = require('fs');
-
+const {Sessions} = require('./models/sessions');
+const {processGithubOauth} = require('./handlers');
 const allRecipes = [];
 
 const app = express();
+
+app.locals.sessions = new Sessions();
 
 app.use((req, res, next) => {
   console.log(req.url);
   next();
 });
 
-app.use(express.urlencoded({extended: true, limit: '50mb'}));
-
 app.use(cookieParser());
 app.use(fileUpload());
 app.use(express.json());
+
 app.use('/api/assets', express.static('public/assets'));
+
+app.get('/api/verify', processGithubOauth);
+
+app.get('/api/isLoggedIn', (req, res) => {
+  const {sessions} = req.app.locals;
+  const {sId} = req.cookies;
+  const userId = sessions.getUserId(sId);
+  if (userId) res.json({status: true});
+  else res.json({status: false});
+});
 
 app.get('/api/getAllRecipes', (req, res) => {
   res.json(allRecipes);
---
+++ src/handlers.js
@@ -0,0 +1,36 @@
+const axios = require('axios');
+
+const generateUserInfoConfig = function(accessToken) {
+  return {
+    url: 'https://api.github.com/user',
+    headers: {
+      Authorization: `token ${accessToken}`,
+      accept: 'application/json'
+    }
+  };
+};
+
+const generateAccessTokenConfig = function(code) {
+  return {
+    method: 'post',
+    url: `https://github.com/login/oauth/access_token?client_id=1d0dd614acec505180d7&client_secret=bc54228f715cc9b4691f8f518c77e8d071657ffb&code=${code}`,
+    headers: {
+      accept: 'application/json'
+    }
+  };
+};
+
+const processGithubOauth = function(req, res) {
+  const {sessions} = req.app.locals;
+  const {code} = req.query;
+  axios(generateAccessTokenConfig(code)).then(({data}) => {
+    const accessToken = data['access_token'];
+    axios(generateUserInfoConfig(accessToken)).then(({data}) => {
+      const {name, avatar_url, id} = data;
+      res.cookie('sId', sessions.createSession(data.id));
+      res.redirect('http://localhost:3000/');
+    });
+  });
+};
+
+module.exports = {processGithubOauth};
