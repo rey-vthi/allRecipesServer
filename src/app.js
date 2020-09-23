e0ddf7170e97209df9c867d17525a7a942a54740
revthi@gmail.com <58027828+rey-vthi@users.noreply.github.com>
2020-09-23 15:19:43 +0530

    added api for getting the request

--- src/app.js
+++ src/app.js
@@ -3,92 +3,54 @@ const fileUpload = require('express-fileupload');
 const cookieParser = require('cookie-parser');
 const fs = require('fs');
 
-const allRecipes = [
-  {
-    by: 'breakfast',
-    name: 'lunch',
-    ingredients: ['2cup rice', '2cup jaggery', 'water'],
-    steps: [
-      'Boil water',
-      'put rice into boiled water',
-      'smash the jaggery and add it to boiled rice'
-    ],
-    description: 'tasty sweaty pongal is made ..',
-    type: 'breakfast'
-  },
-  {
-    by: 'revathi',
-    name: 'salad',
-    ingredients: ['2cup rice', '2cup jaggery', 'water'],
-    steps: [
-      'Boil water',
-      'put rice into boiled water',
-      'smash the jaggery and add it to boiled rice'
-    ],
-    description: 'tasty sweaty pongal is made ..',
-    type: 'salad'
-  },
-  {
-    by: 'revathi',
-    name: 'lunch',
-    ingredients: ['2cup rice', '2cup jaggery', 'water'],
-    steps: [
-      'Boil water',
-      'put rice into boiled water',
-      'smash the jaggery and add it to boiled rice'
-    ],
-    description: 'tasty sweaty pongal is made ..',
-    type: 'lunch'
-  },
-  {
-    by: 'revathi',
-    name: 'juice',
-    ingredients: ['2cup rice', '2cup jaggery', 'water'],
-    steps: [
-      'Boil water',
-      'put rice into boiled water',
-      'smash the jaggery and add it to boiled rice'
-    ],
-    description: 'tasty sweaty pongal is made ..',
-    type: 'juice'
-  }
-];
+const allRecipes = [];
 
 const app = express();
+
+app.use((req, res, next) => {
+  console.log(req.url);
+  next();
+});
+
+app.use(express.urlencoded({extended: true, limit: '50mb'}));
 
 app.use(cookieParser());
 app.use(fileUpload());
 app.use(express.json());
+app.use('/api/assets', express.static('public/assets'));
 
 app.get('/api/getAllRecipes', (req, res) => {
   res.json(allRecipes);
 });
 
 app.get('/api/getAllSalads', (req, res) => {
-  const recipes = allRecipes.filter(r => r.type === 'salad');
+  const recipes = allRecipes.filter(r => r.category === 'salad');
   res.json(recipes);
 });
 
 app.get('/api/getAllJuice', (req, res) => {
-  const recipes = allRecipes.filter(r => r.type === 'juice');
+  const recipes = allRecipes.filter(r => r.category === 'juice');
   res.json(recipes);
 });
 
 app.get('/api/getAllBreakfast', (req, res) => {
-  const recipes = allRecipes.filter(r => r.type === 'breakfast');
+  const recipes = allRecipes.filter(r => r.category === 'breakfast');
   console.log(recipes);
   res.json(recipes);
 });
 
 app.get('/api/getAllLunch', (req, res) => {
-  const recipes = allRecipes.filter(r => r.type === 'lunch');
+  const recipes = allRecipes.filter(r => r.category === 'lunch');
   res.json(recipes);
 });
 
 app.post('/api/addNewRecipe', (req, res) => {
-  const {dishName, ingredients, steps, description} = req.body;
   const {file} = req.files;
   fs.writeFileSync(`./public/assets/${file.md5}.jpg`, file.data, 'utf8');
+  allRecipes.push(
+    Object.assign(req.body, {by: 'revathi', path: `/assets/${file.md5}.jpg`})
+  );
+  res.end();
 });
 
 module.exports = {app};
