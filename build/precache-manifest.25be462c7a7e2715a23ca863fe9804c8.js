1a476606d1a1ad52ff1864742e04134f8395adb1
revthi@gmail.com <58027828+rey-vthi@users.noreply.github.com>
2020-09-30 20:23:15 +0530

    added build

---
+++ build/precache-manifest.25be462c7a7e2715a23ca863fe9804c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a89c766918bd1ee819d887d3ee02bc73",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a69da8e5fece278ec950",
+    "url": "/static/css/main.6df7bd97.chunk.css"
+  },
+  {
+    "revision": "73dc9e093942dbde587b",
+    "url": "/static/js/2.0122a5c9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0122a5c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a69da8e5fece278ec950",
+    "url": "/static/js/main.272d989e.chunk.js"
+  },
+  {
+    "revision": "82eb311ef9f207dfb5b2",
+    "url": "/static/js/runtime-main.7b6bd03a.js"
+  }
+]);
