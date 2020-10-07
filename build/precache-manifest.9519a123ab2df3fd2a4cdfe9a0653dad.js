69c27de30acb386369a6115f9de441b1fbe35296
revthi@gmail.com <58027828+rey-vthi@users.noreply.github.com>
2020-10-07 13:40:49 +0530

    removed build from gitignore

---
+++ build/precache-manifest.9519a123ab2df3fd2a4cdfe9a0653dad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f184eca2f6cabf9c455157060348603",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3206508f05b73d85c17e",
+    "url": "/static/css/main.ebba2336.chunk.css"
+  },
+  {
+    "revision": "73dc9e093942dbde587b",
+    "url": "/static/js/2.0122a5c9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0122a5c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3206508f05b73d85c17e",
+    "url": "/static/js/main.872eff35.chunk.js"
+  },
+  {
+    "revision": "82eb311ef9f207dfb5b2",
+    "url": "/static/js/runtime-main.7b6bd03a.js"
+  }
+]);
